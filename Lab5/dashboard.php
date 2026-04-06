e49117db59e6d3f7e8484447ead5de25985b4c11
Bishwajit <130075034+bishwajit006@users.noreply.github.com>
2026-04-07 00:28:38 +0600

    Add files via upload

---
+++ Lab5/dashboard.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Daraz Clone</title>
+    <link rel="stylesheet" href="dashboard.css">
+</head>
+
+<body>
+
+    <!-- HEADER -->
+    <header class="header">
+        <div class="logo">Daraz</div>
+
+        <div class="search-box">
+            <input type="text" placeholder="Search in Daraz">
+            <button>🔍</button>
+        </div>
+
+        <div class="menu">
+            <a href="#">SAVE MORE ON APP</a>
+            <a href="#">BECOME A SELLER</a>
+            <a href="#">HELP & SUPPORT</a>
+            <a href="#">ACCOUNT</a>
+        </div>
+    </header>
+
+    <!-- MAIN BANNER -->
+    <section class="banner">
+
+        <div class="banner-left">
+            <h4>4.4 SUMMER SALE</h4>
+            <h1>FLASH SALE</h1>
+            <p>Limited Time & Stock</p>
+            <span class="discount">UP TO 80% OFF</span>
+            <button class="btn">Shop Now</button>
+        </div>
+
+        <div class="banner-center">
+<img src="assets/banner.png" alt="Daraz Sale">        </div>
+
+        <div class="banner-right">
+            <div class="app-box">
+                <h3>Download the App</h3>
+                <p>⭐ 4.8 Rated</p>
+                <button class="download">Download App</button>
+
+                <div class="features">
+                    <p>🚚 Free Delivery</p>
+                    <p>⏰ Limited Time</p>
+                </div>
+
+                <img class="qr" src="assets/image2.png">
+            </div>
+        </div>
+
+    </section>
+
+    <!-- LOWER PROMO -->
+    <section class="promo">
+        <h2>4.4 Summer Sale</h2>
+        <h2>SALE IS LIVE NOW</h2>
+        <button class="btn">SHOP NOW</button>
+    </section>
+
+</body>
+
+</html>
